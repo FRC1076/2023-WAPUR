78dbc8899e5aa6810a5757e83ce467505a04b425
Cryplo <dylanliaa@gmail.com>
2023-11-12 15:16:29 -0500

    Add robot.py and robotconfig.py

---
+++ robotconfig.py
@@ -0,0 +1,53 @@
+from collections import namedtuple
+
+DEADZONE = 0.1
+
+# Drive Types
+ARCADE = 1
+TANK = 2
+SWERVE = 3
+
+controllerConfig = {
+    
+}
+
+swervometerConfig = {
+
+}
+
+drivetrainConfig = {
+
+}
+
+visionConfig = {
+
+}
+
+elevatorConfig = {
+
+}
+
+grabberConfig = {
+
+}
+
+autonConfig = {
+
+}
+
+MODULE_NAMES = namedtuple('MODULE_NAMES', [
+])
+
+loggingConfig = {
+
+}
+
+dashboardConfig = {
+
+}
+
+bot = {
+    
+}
+
+robotconfig = bot
