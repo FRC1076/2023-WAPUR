b3bcea73c9ab1b35c4e1439a41fae773c9de8b73
Ashrit Kumar <436598@aaps.k12.mi.us>
2023-12-08 20:23:53 -0500

    defined the pistons and rewrote the motors

--- launcher.py
+++ launcher.py
@@ -3,32 +3,48 @@ import wpilib.drive
 import wpimath.controller
 from wpimath.controller import PIDController
 import rev
+from wpilib import DoubleSolenoid
+import ctre
 
 class Launcher:
     def __init__(self, config):
 
-        motorType = rev.CANSparkMaxLowLevel.MotorType.kBrushless
-        self.topMotor = rev.CANSparkMax(config["TOP_MOTOR_ID"], motorType) # launcher top-bottom
-        self.bottomMotor = rev.CANSparkMax(config["BOTTOM_MOTOR_ID"], motorType) # launcher top-bottom
+        
+        self.topMotor = ctre.TalonSRX(config["TOP_MOTOR_ID"]) # launcher top-bottom
+        self.bottomMotor = ctre.TalonSRX(config["BOTTOM_MOTOR_ID"]) # launcher top-bottom
+        # CAN ID
+        # module type
+        # forward port
+        # backward port, piston configuration name
+        self.ejectPiston =  DoubleSolenoid(config["EJECT_CAN_ID"],config["EJECT_MODULE_TYPE"],
+                                        config["EJECT_FORWARD_CHANNEL"], config["EJECT_REVERSE_CHANNEL"]) # piston responsible for pushing ball out
+        self.aimPiston = DoubleSolenoid(config["AIM_CAN_ID"],config["AIM_MODULE_TYPE"],
+                                        config["AIM_FORWARD_CHANNEL"], config["AIM_REVERSE_CHANNEL"]) # piston responsible for aiming the launcher
 
-        # unsure how pistons work, however this is a similar layout to motorType
-        self.pistonType = # piston configuration name
-        self.ejectPiston =  # piston responsible for pushing ball out
-        self.aimPiston = # piston responsible for aiming the launcher
-                              
+        # uses the motors                  
     def intake(self):
         intakeSpeed = self.config["INTAKE_SPEED"]
         self.topMotor.set(intakeSpeed)
         self.bottomMotor.set(intakeSpeed)
+        
 
+        # uses the pistons
     def eject(self):
         ejectSpeed = self.config["EJECT_SPEED"]
         self.topMotor.set(ejectSpeed)
         self.bottomMotor.set(ejectSpeed)
+        
         # extend ejectPiston
 
     def aimDown(self):
         # retract aimPiston
 
+        self.aimPiston.set(wpilib.DoubleSolenoid.Value.kReverse)
+
     def aimUp(self):
         # extend aimPiston
+
+        self.aimPiston.set(wpilib.DoubleSolenoid.Value.kForward)
+
+
+    
--- robotconfig.py
+++ robotconfig.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import wpilib
 
 DEADZONE = 0.1
 
@@ -37,12 +38,18 @@ visionConfig = {
 launcherConfig = {
     "TOP_MOTOR_ID": 1,
     "BOTTOM_MOTOR_ID": 2,
-    "INTAKE_SPEED": 1,
-    "EJECT_SPEED": 1,
-}
+    "INTAKE_SPEED": 0.1,
+    "EJECT_SPEED": -0.1,
 
-grabberConfig = {
+    "AIM_CAN_ID": 0,
+    "AIM_MODULE_TYPE": wpilib.PneumaticsModuleType.CTREPCM,
+    "AIM_FORWARD_CHANNEL": 0,
+    "AIM_REVERSE_CHANNEL": 1,
 
+    "EJECT_CAN_ID": 0,
+    "EJECT_MODULE_TYPE": wpilib.PneumaticsModuleType.CTREPCM,
+    "EJECT_FORWARD_CHANNEL": 2,
+    "EJECT_REVERSE_CHANNEL": 3,
 }
 
 autonConfig = {
